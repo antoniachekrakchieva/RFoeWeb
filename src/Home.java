bd6d51f3c477034c9c3af75c8f11607c31989248
antoniqhc <antoniq_ch@abv.bg>
2016-06-27 00:01:50 +0300

    Initial commit

---
+++ src/Home.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if (request.getParameter("Login") != null) {
+			RequestDispatcher rs = request.getRequestDispatcher("Login");
+	        rs.include(request, response);
+        } else if (request.getParameter("Register") != null) {
+        	RequestDispatcher rs = request.getRequestDispatcher("Register");
+            rs.forward(request, response);
+        }
+	}
+
+}
