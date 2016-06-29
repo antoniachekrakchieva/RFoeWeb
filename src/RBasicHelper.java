5f215014aafe4ffb1e81ba8f34e8f339dfade6c2
antoniqhc <antoniq_ch@abv.bg>
2016-06-29 21:55:53 +0300

    update basic helper

--- src/RBasicHelper.java
+++ src/RBasicHelper.java
@@ -1,9 +1,24 @@
 import java.util.Arrays;
 
 public class RBasicHelper {
-	static String getMeanNumber(String str){
+	static String getMean(String str){
 		str = str.replaceAll("[^-?0-9]+", " "); 
+		for (int i=0; i< str.length(); i++) {
+		    str.charAt(i);
+		}
 		String strI = Integer.toString(str.length());
 		return strI;
 	}
+	
+	static String getSum(String str){
+		str = str.replaceAll("[^-?0-9]+", " "); 
+		int sum = 0;
+		for (int i=0; i< str.length(); i++) {
+			sum += Integer.parseInt(str, str.charAt(i));
+			System.out.println(sum);
+		}
+		String strI = Integer.toString(sum);
+		return str;
+	}
+	
 }
